bb76f819ae685ee539990ed5bac218849d514f57
CancelOnly <70231421+CancelOnly@users.noreply.github.com>
2023-11-09 22:46:29 -0300

    timing do carrossel

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 10000); // Change image every 10 seconds
+    setTimeout(showSlides, 2000); // Change image every 10 seconds
 }
 
 
